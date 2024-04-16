6f41d8fb70dec9d5b20c6805239b0f6ba8f6d02d
Usuario <Usuario@CPD03-M10>
2024-04-16 11:03:14 -0300

    Adicionar arquivos de projeto.

---
+++ DespesaCasa/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DespesaCasa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String resp, despesa;
+            double valor, total = 0;
+
+            do
+            {
+                Console.WriteLine("Informe a despesa: ");
+                despesa = Console.ReadLine();
+
+                Console.WriteLine("Informe o valor: ");
+                valor = double.Parse(Console.ReadLine());
+
+                total += valor;
+
+                Console.WriteLine("Deseja inserir uma nova despesa?");
+
+            }
+        }
+    }
+}
